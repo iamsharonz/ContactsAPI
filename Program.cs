0216932075b89a12cc01f46516553500cbbb45a2
Sharon joseph <sharon.joseph@thinkpalm.com>
2023-10-17 10:28:40 +0530

    Added the PostgressDb

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var db = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ContactsAPIDbContext>(options =>
+ options.UseNpgsql(db));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
